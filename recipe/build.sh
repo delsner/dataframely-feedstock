1f8c9c5e402aff1cf7f9c22715489cb03d26eb38
Daniel Elsner <daniel.elsner@quantco.com>
2026-04-27 10:03:14 +0200

    Add comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
+# Remove this wrapper once https://github.com/conda-forge/rust-activation-feedstock/pull/79 is merged
 mkdir -p "${BUILD_PREFIX}/bin"
 cp "${RECIPE_DIR}/cargo-auditable-wrapper.sh" "${BUILD_PREFIX}/bin/cargo-auditable-wrapper"
 export CARGO="cargo-auditable-wrapper"
@@ -14,4 +15,4 @@ cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
 
 # Build
 python -m pip install . \
-    --no-deps --ignore-installed -vv --no-build-isolation --disable-pip-version-check
+  --no-deps --ignore-installed -vv --no-build-isolation --disable-pip-version-check
